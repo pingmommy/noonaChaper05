9e45ae791d27567182e8c50f65094f4ffeb6cdf9
pingmommy <mcar8764@naver.com>
2024-06-01 01:10:33 +0900

    자바스크립트를 활용하여  기능추가

---
+++ main.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const c1b1 = document.getElementById("c1b1");
+  const c4b1 = document.getElementById("c4b1");
+  const c4b2 = document.getElementById("c4b2");
+  const c3Children01 = document.getElementById("c3-children-box");
+  const c3MotherBox = document.getElementById("c3-mother-box");
+  const c4Event = document.getElementById("c4-event");
+
+  c1b1.addEventListener("click", () => {
+    c1b1.classList.add("left");
+    c1b1.textContent = "BOX1";
+  });
+
+  c3MotherBox.addEventListener("click", () => {
+    c3Children01.style.left = "0px";
+  });
+
+  c4Event.addEventListener("click", () => {
+    c4b2.style.left = "70px";
+    c4b1.style.left = "0px";
+    c4Event.style.visibility = "hidden";
+  });
+});
